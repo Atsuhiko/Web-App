f1e317fed338dc316640732cdce3f3b78c8fef73
Atsuhiko <40190732+Atsuhiko@users.noreply.github.com>
2020-04-28 09:35:22 +0900

    Add files via upload

--- FlaskApp/analyzer/run.py
+++ FlaskApp/analyzer/run.py
@@ -82,4 +82,4 @@ def view(pk):
  
  
 if __name__ == '__main__':
-    app.run(app.run(debug=True, host='0.0.0.0', port=80)) # ポートの変更
+    app.run(app.run(debug=True, host='0.0.0.0', port=8000)) # ポートの変更
