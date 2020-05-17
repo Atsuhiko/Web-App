d677ebf84b16bb64e20ba223ea4728c2621d4731
Atsuhiko <40190732+Atsuhiko@users.noreply.github.com>
2020-05-17 22:21:53 +0900

    Add files via upload

--- practice/MVP/MVP8/app.py
+++ practice/MVP/MVP8/app.py
@@ -18,8 +18,20 @@ from image_process import Predict_Dogs_Cats
 # DBライブラリーインストール
 import db
 
-
 app = Flask(__name__)
+
+jinja_options = app.jinja_options.copy()                                         
+jinja_options.update({                                                      
+    'block_start_string': '<%',                                                 
+    'block_end_string': '%>',                                                   
+    'variable_start_string': '<<',                                              
+    'variable_end_string': '>>',                                                
+    'comment_start_string': '<#',                                               
+    'comment_end_string': '#>'                  
+})                                                                               
+app.jinja_options = jinja_options 
+
+
 app.config.from_object(__name__)
  
 app.config.update(dict(
