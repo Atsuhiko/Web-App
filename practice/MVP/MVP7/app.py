885837a8a0a533f91e33f58507e11fb979cf7240
Atsuhiko <40190732+Atsuhiko@users.noreply.github.com>
2020-06-14 14:55:49 +0900

    Add files via upload

--- practice/MVP/MVP7/app.py
+++ practice/MVP/MVP7/app.py
@@ -102,4 +102,4 @@ def close_db(error):
 
 
 if __name__ == '__main__':
-    app.run(app.run(debug=True,  host='0.0.0.0', port=8080)) # ポートの変更
+    app.run(debug=True,  host='0.0.0.0', port=8080) # ポートの変更
