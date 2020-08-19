6aef3b3569c7f6221115015fc6b215580bede59f
yuyuyuriko78 <yuyuyuriko78@gmail.com>
2020-08-19 21:23:34 +0900

    mvp11heroku failed

--- practice/MVP/MVP11heroku/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists results;
-create table results (
-  `id` integer primary key autoincrement,
-  `filepath` text not null,
-  `prediction` text not null,
-  `dog` real,
-  `cat` real,
-  `created` datetime default CURRENT_TIMESTAMP
-);
